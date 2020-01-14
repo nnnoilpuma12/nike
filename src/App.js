f47ed1a79b6b81ef9b26418a5cc3c4e0fb61f531
nnnoilpuma12 <nnnoilpuma12@gmail.com>
2020-01-14 22:40:34 +0900

    change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ToDoListItem from "./ToDoListItem.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // ToDoListをstateに定義、初期値は[]
+  state = {
+    todoList: []
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form className="App-form"
+        onSubmit={e => {
+          // formのデフォルトのイベントをキャンセル
+          e.preventDefault();
+
+          // idがtitleのElementを取得
+          const titleElement = e.target.elements["title"]
+          // idがdescriptionのElementを取得
+          const descriptionElement = e.target.elements["description"];
+
+          // todoList stateに追加
+          this.setState(
+            {
+              todoList: this.state.todoList.concat({
+                title: titleElement.value,
+                description: descriptionElement.value
+              })
+            },
+
+            // stateの変更後に入力した値を空にする
+            () => {
+              titleElement.value = "";
+              descriptionElement.value = "";
+            })
+          }}
+          >
+          <div>
+            <input id="title" placeholder="title"/>
+            <textarea id="desctiption" placeholder="description"/>
+          </div>
+          <div>
+            <button type="submit">
+              登録
+            </button>
+          </div>
+        </form>
+        <div>
+          
+       <ToDoListItem
+            title="ホームページ作成"
+            description="サークルホームページを今週中に作成する"
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
